36907a9b72825dac076aaa6d3e3913dc33f9e6dd
Amy Fox <amyfox723@gmail.com>
2024-02-23 11:28:27 -0700

    add edit and delete buttons

--- FilmCollection/Controllers/HomeController.cs
+++ FilmCollection/Controllers/HomeController.cs
@@ -45,5 +45,41 @@ namespace FilmCollection.Controllers
 
             return View(submissions);
         }
+
+        [HttpGet]
+        public IActionResult Edit (int id)
+        {
+            var recordToEdit = _context.Submissions
+                .Single(x => x.SubmissionID == id);
+
+            return View("Form", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit (Submission updatedInfo) 
+        {
+            _context.Update(updatedInfo);
+            _context.SaveChanges(); 
+
+            return RedirectToAction("MovieList");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Submissions
+                .Single(x => x.SubmissionID == id);
+
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Submission submission)
+        {
+            _context.Submissions.Remove(submission);
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieList");
+        }
     }
 }
